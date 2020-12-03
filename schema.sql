dcbfd15e1e1a9041fa02ee534468608734bc8d67
dansirdan <daniel.monteton@gmail.com>
2020-12-03 12:17:16 -0800

    updated sql schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,16 @@
+drop table if exists users, withlists, destinations, ratings, withlistusers;
+
 drop table if exists users;
 create table users (
 	user_id 					SERIAL primary key,
 	username 					VARCHAR(100),
 	user_email 					VARCHAR(100),
-	user_password 				VARCHAR(100),
+	user_salt					BYTEA,
+	user_password 				VARCHAR(255),
+	is_admin					BOOLEAN default false,
 	user_photo 					VARCHAR(100)
 );
 
-select * from users;
 
 drop table if exists withlists;
 create table withlists (
@@ -21,7 +24,7 @@ create table withlists (
 			on delete cascade
 );
 
-select* from withlists;
+--select* from withlists;
 
 drop table if exists destinations;
 create table destinations (
@@ -38,7 +41,21 @@ create table destinations (
 			on delete cascade
 );
 
-select * from destinations;
+drop table if exists withlistusers;
+create table withlistusers (
+	member_id					SERIAL primary key,
+	user_id 					INT,
+	withlist_id 				INT,
+	constraint fk_withlist
+	foreign KEY(withlist_id)
+		references withlists(withlist_id)
+		on delete cascade,
+	constraint fk_user
+	foreign KEY(user_id)
+		references users(user_id)
+		on delete cascade
+);
+--select * from destinations;
 
 drop table if exists ratings;
 create table ratings (
@@ -52,28 +69,10 @@ create table ratings (
 		on delete cascade,
 	constraint fk_user
 	foreign KEY(user_id)
-		references users(user_id)
+		references withlistusers(member_id)
 		on delete cascade
 );
 
-select * from ratings;
-
-drop table if exists withlistusers;
-create table withlistusers (
-	withlistuser_id 			SERIAL primary key,
-	withlist_id 				INT,
-	user_id 					INT,
-	constraint fk_withlist
-	foreign KEY(withlist_id)
-		references withlists(withlist_id)
-		on delete cascade,
-	constraint fk_user
-	foreign KEY(user_id)
-		references users(user_id)
-		on delete cascade
-);
-
-select * from withlistusers;
 
 insert into users (username, user_email, user_password, user_photo)
 VALUES ('user1', 'user1@email.com', '2222', 'photoplaceholder'),
@@ -81,20 +80,20 @@ VALUES ('user1', 'user1@email.com', '2222', 'photoplaceholder'),
 ('user3', 'user3@email.com', '3334', 'photoplaceholder');
 
 insert into withlists (owner_id, withlist_title, withlist_description)
-VALUES (1, 'Hiking', 'Hiking list to track hiking destinations.'),
+VALUES (1, 'Swimming', 'Got to get all those swims in.'),
 (2, 'Vacation', 'Vacation list to track vacation destinations.'),
 (1, 'Roadtrip 2020', 'Roadtrip list to track roadtrip destinations.');
 
 insert into destinations (withlist_id, dest_name, dest_description, dest_photo)
-VALUES (1, 'Mount Rainier', 'hike to the top', 'placeholder'),
-(1, 'Trailhead1', 'hike to the midpoint', 'placeholder'),
+VALUES (3, 'Trailhead1', 'hike to the midpoint', 'placeholder'),
 (1, 'Northshore', 'hike to the beach', 'placeholder'),
 (2, 'Hawaii', '2021 Christmas', 'placeholder'),
 (2, 'Colorado', 'home to visit parents', 'placeholder');
 
 insert into withlistusers (withlist_id, user_id)
-VALUES (1,1), (1,3);
+VALUES 
+(1,1),(1,2),(2,2), (3,3);
 
 insert into ratings (destination_id, user_id, rating_value)
-VALUES (1, 1, 4.50),
-(1,2,5.00);
+VALUES 
+(2,1,4.00);
