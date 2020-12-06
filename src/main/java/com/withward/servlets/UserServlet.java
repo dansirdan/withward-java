c4687141de0b4643d07774a92619db90615ea293
dansirdan <daniel.monteton@gmail.com>
2020-12-05 17:27:11 -0800

    testing

--- src/main/java/com/withward/servlets/UserServlet.java
+++ src/main/java/com/withward/servlets/UserServlet.java
@@ -155,7 +155,7 @@ public class UserServlet extends HttpServlet {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			res.setStatus(400);
-			res.getWriter().append(System.getenv("DB_NAME"));
+			res.getWriter().append(System.getProperty("DB_NAME"));
 			e.printStackTrace();
 		}
 
